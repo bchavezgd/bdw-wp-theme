80e167bc0392b3dcc937363842f8e0ce8321f39b
bchavezgd <bchavez.gd@gmail.com>
2014-07-19 12:54:12 -0700

    widget
    added code for registering widget, can't tell if i did it right or not.

--- sidebar.php
+++ sidebar.php
@@ -5,6 +5,8 @@
 			if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : 
 			endif;
 		?>  
+		<?php if (!dynamic_sidebar('Primary Widget Area')) : ?>
+		<?php endif; ?>
 	</ul>
 </aside>
 <!-- end sidebar -->
