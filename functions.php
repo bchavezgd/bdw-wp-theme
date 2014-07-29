794f38e78e377394e4cc37013355a3ed67e962a9
bchavezgd <bchavez.gd@gmail.com>
2014-07-28 22:09:12 -0700

    custom post types
    added support for "generic content" post type.

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <!-- functions.php -->
 <?php 
 	if(function_exists('register_sidebar'))
+		// adds area for wordpress widgets. 
 		register_sidebar(
 			array(
 				'name' => _('Primary Widget Area'),
@@ -8,19 +9,38 @@
 				'description' =>  _('The primary widget area'),
 				'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
 				'after_widget' => '</li>',
-				'before_title' => '<h3 class="widet-title">',
+				'before_title' => '<h3 class="widget-title">',
 				'after_title' => '</h3>', 
 			) ); 
+
+// creates second widget
 		register_sidebar(
 			array(
 				'name' => 'Second Widget Area',
 			));
+
+// registers a "nav_menu" to be called into theme customization 
 		register_nav_menu(
 			'primary', __(
 				'Navigation Menu', 'twentythirteen'
 			));
+// create an area in the "customize" section for a nav menu. 
+// adds theme support
 		add_theme_support('nav-menus');
-
+	  
+// creating custom post types. 
+	add_action('init','create_post_type');
+		function create_post_type(){
+			register_post_type('generic-content', //defaults to single.php, need to add single-generic-content.php to for custom template. 
+				array(
+					'labels' => array(
+						'name' => __('Generic Content'),
+						'singular_name' => __('Generic Content')
+						),
+					'public' => true
+				)
+			);
+		}// why are there brackets here but no where else. 
 
 
 // end functions.php 
