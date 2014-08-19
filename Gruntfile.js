2770799c265906776e160b1f25bc421ac9c834db
bchavezgd <bchavez.gd@gmail.com>
2014-08-18 22:57:57 -0700

    new gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,48 @@
 module.exports = function (grunt) {
 
-// Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'js/surprise.js',
-                dest: 'js/build/<%= pkg.name %>.min.js'
-            }
-        },
-        compass: {
-            dist: {
-                options: {
-                    config: 'sass/config.rb'
-                }
-            }
-        },
-        watch: {
-            scripts: {
-                files: ['sass/*.sass'],
-                tasks: ['compass'],
-                options: {
-                    spawn: false
-                }
-            }
-        }
-    });
+  // Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+	// load plugin options and stuff. 
+		watch: {
+			css: {
+				files: 'sass/*.sass',
+				tasks: ['compass','concat']
+			}
+		},
+		uglify: {},
+		imagemin: {},
+		compass: {
+			dist: {
+				options: {
+					config: 'config.rb'
+				}
+			}
+		},
+		concat: {
+			options: {
+				separator: '\n'
+			},
+			dist: {
+				src: ['stylesheets/wp-banner.css','stylesheets/screen.css'], 
+				dest: 'style.css'
+			}
+		}
+	});
 
-// Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    
-// Default task(s).
-    grunt.registerTask('default', ['uglify', 'compass']);
+  // Load plugins for tasks
+	// uglify
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	// compass
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	// imagemin
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	// concat
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	// watch
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+	grunt.registerTask('default', ['uglify','compass:dev']);
 
 };
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ BDW Theme
 
 Learning to create Wordpress themes, and customizations for the greater good.
 
-Currently working though books, to make basic unstyled theme using compass/sass, and brackets text editor.
+Currently working though books, to make basic unstyled theme using compass/sass, Grunt, and brackets text editor.
 
 ------
 ##Change Log:
@@ -25,29 +25,33 @@ Currently working though books, to make basic unstyled theme using compass/sass,
 - created `category-cars.php`
 	- demo only
 - added initial styling. 
+- implimented Gruntfile.js
+	- watch
+		- currently runs compass then concat for style.css
 
---------
+**Media Library** *fixed*  
 
-## Planned: 
-- correctly create Style.sass so that compass will create the correct style.css 
-- impliment **Grunt.js**
-	- install `node.js`
-	- figure out Grunt/Compass workflow
-- add stylesheets for media queries. 
-	- tablet (portrait) 
-	- phone
-
----
-## Bugs: 
-### Media Library 
 - not loading any images when trying to add media to a post/page. 
 	- Media library from dashboard works fine when added or editing images. 
 	- deactivated all plugins, nothing changed. 
 	- changed to default twentyfourteen theme, works fine. problem exists in my theme. 
-#### fix attempts. 
-1. removed functions.php from root folder of theme, and media library worked fine. 
-	- issue is in functions.php for sure. 
-	- removed comments. 
-	- removed white space. 
-	- rearranged `generic content` section. 
-		- corrected issue. 
+	- **fix attempts. **
+
+		1. removed functions.php from root folder of theme, and media library worked fine.
+			- removed comments. no change
+			- removed white space. no change 
+			- rearranged `generic content` section. 
+				- corrected issue. 
+
+--------
+
+## Planned: 
+- add stylesheets for media queries. 
+	- tablet (portrait) 
+	- phone
+- learn muliple loops. 
+	- add featured loop and catagory loops. 
+- single.php doesn't show content. 
+
+---
+## Bugs: 
