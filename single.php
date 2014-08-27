f22cbe7191811c1011fc2a39ae9530dbffcaf6ff
bchavezgd <bchavez.gd@gmail.com>
2014-08-26 21:12:14 -0700

    vertical rhythm play

--- single.php
+++ single.php
@@ -1,36 +1,31 @@
 <?php // single.php
 get_header(); ?>
 <hr>
-    <article class="entry" >
-        <h3 class="title"><?php the_title(); ?></h3>
-       
-        <section class="entry-content">
-			
-            <div class="entrybody">
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<?php the_content(__('Read more'));?>
-
-<p class="blogcategory">Categories: <?php the_category(' &bull; '); ?></p>
-<p class="blogtags"><?php the_tags(); ?> </p> 
-
-<?php edit_post_link(__('<strong>Edit</strong>'));
+<article class="entry" >
+	<section class="content cf">
+		<h3 class="title"><?php the_title(); ?></h3>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<?php the_content();?>
+	</section><!-- .entry-content -->
+		<p class="blogcategory">Categories: <?php the_category(' &bull; '); ?></p>
+		<p class="blogtags"><?php the_tags(); ?> </p> 
+		<?php 
+			edit_post_link(__('<strong>Edit</strong>'));
 				endwhile;
 				endif;
-				?>
-				
-</div>
-            <?php
-				wp_link_pages( 
-                    array(
-                    'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bdw' ) . '</span>',
-                    'after'       => '</div>',
-                    'link_before' => '<span>',
-                    'link_after'  => '</span>',
-                ) );
-            ?>
-	</section><!-- .entry-content -->
-
-    </article>
+		?>
+	
+	<div class="clear"></div>
+	<?php
+		wp_link_pages( 
+			array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bdw' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+		));
+	?>
+</article>
 <hr>
 <?php
     get_sidebar();
